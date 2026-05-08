6f10e75390b8a476a1e5112c0f75396cdc254c1e
GustavoRV08 <gustavorv102@gmail.com>
2026-05-08 15:58:43 -0300

    Update main.py

--- Domínio do espaço/main.py
+++ Domínio do espaço/main.py
@@ -4,59 +4,104 @@ import OpenGL.GLUT as glut
 import random
 import math
 
-passos = 100
-angulo = 3.1415926 * 2.0/passos
+class Pontos:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
 
+class Faces:
+  def __init__(self):
+    self.pontos = []
+    self.r = 0
+    self.g = 0
+    self.b = 0
+    self.a = 0
+
+  def add_ponto(self, ponto):
+    self.pontos.append(ponto)
+
+  def set_r(self, r):
+    self.r = r
+
+  def set_g(self, g):
+    self.g = g
+
+  def set_b(self, b):
+    self.b = b
+
+  def set_a(self, a):
+    self.a = a
+
+triangulos = []
+  
 def display():
-  glut.glutSwapBuffers()
   gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-  #circulos
   gl.glShadeModel(gl.GL_FLAT)
-  posX = 0
-  posY = 0
-  raio = 0.3
-  antX = posX
-  antY = posY - raio
-  for i in  range(1, passos+1):
-    novoX = raio * math.sin(angulo*i)
-    novoY = -raio * math.cos(angulo*i)
-    gl.glBegin(gl.GL_TRIANGLES)
-    gl.glColor3f(1, 0, 0)
-    gl.glVertex3f(0, 0, 0)
-    gl.glVertex3f(antX, antY, 0)
-    gl.glVertex3f(novoX, novoY, 0)
-    gl.glEnd()
-    antX = novoX
-    antY = novoY
-  #triangulo
+  gl.glBegin(gl.GL_TRIANGLES)
+  gl.glColor3f(1, 0, 0)
+  gl.glVertex3f(triangulos[0].pontos[0].x, triangulos[0].pontos[0].y, 0)
+  gl.glVertex3f(triangulos[0].pontos[1].x, triangulos[0].pontos[1].y, 0)
+  gl.glVertex3f(triangulos[0].pontos[2].x, triangulos[0].pontos[2].y, 0)
+  gl.glEnd()
+
   gl.glShadeModel(gl.GL_FLAT)
   gl.glBegin(gl.GL_TRIANGLES)
   gl.glColor3f(0, 1, 0)
-  gl.glVertex3f(-0.7, 0.0, 0.0)
-  gl.glVertex3f(-0.7, 0.5, 0)
-  gl.glVertex3f(-0.5, 0.0, 0)
-  gl.glEnd()
-  #quadrado
-  gl.glShadeModel(gl.GL_FLAT)
-  gl.glBegin(gl.GL_QUADS)
-  gl.glColor3f(0, 0, 1)
-  gl.glVertex3f(0.4, 0, 0)
-  gl.glVertex3f(0.4, 0.4, 0)
-  gl.glVertex3f(0.8, 0.4, 0)
-  gl.glVertex3f(0.8, 0.0, 0)
-  gl.glEnd()
-  #forma extra
-  gl.glShadeModel(gl.GL_FLAT)
-  gl.glBegin(gl.GL_POLYGON)
-  gl.glColor3f(1, 0, 1)
-  gl.glVertex3f(0.9, 0.0, 0)
-  gl.glVertex3f(0.9, 0.5, 0)
-  gl.glVertex3f(0.94, 0.54, 0)
-  gl.glVertex3f(0.98, 0.5, 0)
-  gl.glVertex3f(0.98, 0, 0)
-  gl.glVertex3f(0.94, -0.05, 0)
+  gl.glVertex3f(triangulos[1].pontos[0].x, triangulos[1].pontos[0].y, 0)
+  gl.glVertex3f(triangulos[1].pontos[1].x, triangulos[1].pontos[1].y, 0)
+  gl.glVertex3f(triangulos[1].pontos[2].x, triangulos[1].pontos[2].y, 0)
   gl.glEnd()
 
+  gl.glShadeModel(gl.GL_FLAT)
+  gl.glBegin(gl.GL_TRIANGLES)
+  gl.glColor3f(0, 0, 1)
+  gl.glVertex3f(triangulos[2].pontos[0].x, triangulos[2].pontos[0].y, 0)
+  gl.glVertex3f(triangulos[2].pontos[1].x, triangulos[2].pontos[1].y, 0)
+  gl.glVertex3f(triangulos[2].pontos[2].x, triangulos[2].pontos[2].y, 0)
+  gl.glEnd()
+
+  gl.glShadeModel(gl.GL_FLAT)
+  gl.glBegin(gl.GL_LINES)
+  gl.glColor3f(1, 1, 1)
+  gl.glVertex3f(-1, 0, 0)
+  gl.glVertex3f(1, 0, 0)
+  gl.glEnd()
+
+  gl.glShadeModel(gl.GL_FLAT)
+  gl.glBegin(gl.GL_LINES)
+  gl.glColor3f(1, 1, 1)
+  gl.glVertex3f(0, -1, 0)
+  gl.glVertex3f(0, 1, 0)
+  gl.glEnd()
+  glut.glutSwapBuffers()
+  
+
+face1 = Faces()
+p1 = Pontos(-0.7, 0.2)
+p2 = Pontos(-0.7, 0.7)
+p3 = Pontos(-0.4, 0.2)
+face1.pontos.append(p1)
+face1.pontos.append(p2)
+face1.pontos.append(p3)
+triangulos.append(face1)
+
+face2 = Faces()
+p1 = Pontos(0.7, 0.2)
+p2 = Pontos(0.7, 0.7)
+p3 = Pontos(0.4, 0.2)
+face2.pontos.append(p1)
+face2.pontos.append(p2)
+face2.pontos.append(p3)
+triangulos.append(face2)
+
+face3 = Faces()
+p1 = Pontos(-0.7, -0.2)
+p2 = Pontos(-0.7, -0.7)
+p3 = Pontos(-0.4, -0.2)
+face3.pontos.append(p1)
+face3.pontos.append(p2)
+face3.pontos.append(p3)
+triangulos.append(face3)
 
 glut.glutInit()
 glut.glutInitDisplayMode(0)
